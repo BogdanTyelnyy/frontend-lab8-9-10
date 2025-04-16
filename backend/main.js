13fc7cf9db196becc95caf159579c399a843dea4
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-04-16 22:43:58 +0300

    added react-router

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,5 @@
 import express from "express";
-const PORT = 3000;
+const PORT = 5000;
 const HOST = 'localhost';
 const app = express();
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
     return (
-        <>
-        </>
+        <Routes>
+            <Route path="/" element={<div>Home</div>} />
+            <Route path="/test" element={<div>Test</div>} />
+        </Routes>
     );
 }
 
