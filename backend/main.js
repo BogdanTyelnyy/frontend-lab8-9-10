65ada67f7fcfede30562db19df613a9bb88d7646
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-04-16 22:58:05 +0300

    added cors for backend

--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,16 @@
 import express from "express";
+import cors from "cors";
 const PORT = 5000;
 const HOST = 'localhost';
 const app = express();
 
+app.use(cors());
+
 app.listen(PORT, HOST, () => {
     console.log(`Server is listening on ${HOST}:${PORT}`);
+});
+
+app.get('/films', (req, res) => {
+    console.log('here');
+    res.status(200).json([]);
 });
