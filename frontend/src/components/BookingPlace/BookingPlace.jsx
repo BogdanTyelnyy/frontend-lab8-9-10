6b9e70856a0a0228df90201b97003183544379d7
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-04-17 15:44:40 +0300

    created BookingPlace component

---
+++ frontend/src/components/BookingPlace/BookingPlace.jsx
@@ -0,0 +1,8 @@
+import "./BookingPlace.css";
+export default function BookingPlace({ text }) {
+    return (
+        <div className="booking-place">
+            { text }
+        </div>
+    );
+}
