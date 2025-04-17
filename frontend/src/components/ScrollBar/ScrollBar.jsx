c327c96b43a17f52a2a02a0e2d2426c06b5cf6b1
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-04-17 15:26:41 +0300

    added ScrollBar component

---
+++ frontend/src/components/ScrollBar/ScrollBar.jsx
@@ -0,0 +1,9 @@
+export default function ScrollBar({handlePrev, handleNext, text}) {
+    return (
+        <div className="scroll-bar">
+            <button onClick={handlePrev}>{"<"}</button>
+            <p>{text}</p>
+            <button onClick={handleNext}>{">"}</button>
+        </div>
+    );
+}
