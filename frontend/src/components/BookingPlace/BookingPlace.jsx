a8884260ec4d42eed4ea9dad2ba0e6cadb784aa2
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-04-22 20:59:11 +0300

    state influence on className in BookingPlace

--- frontend/src/components/BookingPlace/BookingPlace.jsx
+++ frontend/src/components/BookingPlace/BookingPlace.jsx
@@ -1,7 +1,12 @@
+import { useRef } from "react";
 import "./BookingPlace.css";
-export default function BookingPlace({ text }) {
+export default function BookingPlace({ text, state, handleClick }) {
+    const ref = useRef();
     return (
-        <div className="booking-place">
+        <div 
+            className={`booking-place ${state}`}
+            ref={ref} 
+            onClick={handleClick}>
             { text }
         </div>
     );
