9c5e706b02e656279f9175801a319c6845c507ba
Bogdan Tielnyi <bogdan.tlny@gmail.com>
2025-04-22 23:26:56 +0300

    adjusted modal window to confirm booking places

--- frontend/src/components/ConfirmBooking/ConfirmBooking.jsx
+++ frontend/src/components/ConfirmBooking/ConfirmBooking.jsx
@@ -2,22 +2,50 @@ import { useRef } from "react";
 import "./ConfirmBooking.css";
 export default function ConfirmBooking({ hide, confirm }) {
     const ref = useRef();
-    function handleClick(e) {
+    function handleOutsideClick(e) {
         if(e.target === ref.current) {
             hide();
         }
     }
+    function check(e) {
+        const form = e.target;
+        if(form[0].value === '') 
+            throw form[0];
+        if(form[1].value === '')
+            throw form[1];
+        if(form[2].value === '')
+            throw form[2];
+        for(let i of form[1].value) {
+            if(!('0123456789'.includes(i))) throw form[1];
+        }
+        return true;
+    }
+    function handleConfirmation(e) {
+        e.preventDefault();
+        try {
+            if(check(e)) {
+                confirm();
+            }
+        } catch(err) {
+            err.style.border = "1px solid red";
+            setTimeout(() => {
+                err.style.border = "";
+            }, 1000);
+        }
+    }
     return (
-        <div 
+        <form
             ref={ref}
+            onSubmit={handleConfirmation}
             className="confirm-booking-overlay"
-            onClick={handleClick}>
+            onClick={handleOutsideClick}>
             <div className="confirm-booking">
-                <input></input>
-                <input></input>
-                <input></input>
-                <button onClick={confirm}>Confirm</button>
+                <strong>Input your data:</strong>
+                <input type="text" placeholder="Name..."></input>
+                <label>+38<input type="tel" placeholder="XXXXXXXXXX"></input></label>
+                <input type="email" placeholder="example@mail.com"></input>
+                <button type="submit">Confirm</button>
             </div>
-        </div>
+        </form>
     );
 }
